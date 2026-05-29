c8e47f6535979c1b66058cad935040e7feca3cdb
Dhanesh Palekar <dhaneshpalekar21@gmail.com>
2026-05-29 13:05:56 +0530

    connect frontend to render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://agroerp-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
